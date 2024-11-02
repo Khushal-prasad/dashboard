465a5ba15e7507d6aa56e2e7931ed2431901c88f
Khushal-prasad <jeetuprasad277@gmail.com>
2024-11-02 17:51:25 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,9 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-  <BrowserRouter basename="/beck.kap">
-    <App />
-  </BrowserRouter>
+    <BrowserRouter basename="/beck.kap">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
   
 )
